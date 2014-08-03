ce8683565507c11442bf572e9e733e995b07c6d4
Eric Spector <eric.w.spector@gmail.com>
2014-08-03 02:13:46 -0400

    package cleanup

--- eric-mac-bootstrap.sh
+++ eric-mac-bootstrap.sh
@@ -16,72 +16,16 @@ xcode-select --install
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew doctor
 
-#install command line utilities
-brew install ffmpeg
-brew install highlight
-brew install imagemagick
-brew install phantomjs
-brew install wget
-brew install python
-brew install vim --env-std --override-system-vim
+#install brew packages
+brew bundle Brewinstall
 
-#install cask
-brew tap caskroom/cask
-brew install caskroom/cask/brew-cask
-
-#install browsers
-brew cask install google-chrome
-brew cask install firefox
-
-#install cloud services
-brew cask install dropbox
-brew cask install google-drive
-brew cask install evernote
-brew cask install skitch
-brew cask install amazon-music
-
-#install misc applications
-brew cask install adobe-reader
-brew cask install caffeine
-brew cask install colloquy
-brew cask install cyberduck
-brew cask install deluge
-brew cask install fluid
-brew cask install flux
-brew cask install joinme
-brew cask install logitech-myharmony
-brew cask install mame 
-brew cask install mucommander
-brew cask install openemu
-brew cask install pdfsam
-brew cask install picasa
-brew cask install the-unarchiver
-
-#install media tools
-brew cask install calibre
-brew cask install handbrake
-brew cask install vlc
-
-#install dev tools
-brew cask install dash
-brew cask install github
-brew cask install iterm2
-brew cask install sublime-text
-brew cask install sequel-pro
-brew cask install xquartz
-brew cask install virtualbox
-brew cask install vagrant
-brew cask install wineskin-winery
-
-brew install ios-sim
+#install brew cask packages
+brew bundle Caskinstall
 
 #remove downloaded files
 brew cask cleanup
 
-#install zsh as default shell
-brew install zsh
-brew install zsh-completions
-brew install zsh-syntax-highlighting
+#install ohmyzsh
 sudo -s -- "echo /usr/local/bin/zsh >> /etc/shells"
 chsh -s /usr/local/bin/zsh
 #install ohmyzsh
