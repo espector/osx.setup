e92ede1a8f8b0efd72763cdc84cf3e86bedfdc28
espector <eric.w.spector@gmail.com>
2014-08-23 13:34:17 -0400

    added option to update cask and temporarily commented out pia

--- update-packages.sh
+++ update-packages.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
 brew install `brew outdated`
+
+#update cask
+for c in `brew cask list`; do ! brew cask info $c | grep -qF "Not installed" || brew cask install $c; done
